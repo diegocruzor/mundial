e2d83cae3f9b7e8b66bf383a062b3228b037ead1
diegocruzor <diego.cruz@comunidad.iush.edu.co>
2022-11-27 12:08:19 -0500

    Quitar opciones de composer.json

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'e516ef33c96d93c25eaef0efcc79f80f03a95213',
+        'reference' => 'e4d752f85c0dd4bb07ee7aa3b6cdd390ce385520',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'e516ef33c96d93c25eaef0efcc79f80f03a95213',
+            'reference' => 'e4d752f85c0dd4bb07ee7aa3b6cdd390ce385520',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
